217c54013c4497b3330282e9cb00f3c0af6ce4b4
SMRsaimon <smrsaimon960@gmail.com>
2021-05-28 05:48:22 -0700

    branch created by saimon

---
+++ src/components/developerSaimon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const developerSaimon = () => {
+    return (
+        <div>
+            <h1>Hellow</h1>
+        </div>
+    );
+};
+
+export default developerSaimon;
