de67f6b409382818bd2c0031556f9a9a68339b5a
Tanvir Hossain Robin <tanvirhossain88221@gmail.com>
2021-05-29 20:59:04 +0600

    view details

--- src/components/Home/Shop/Shop.jsx
+++ src/components/Home/Shop/Shop.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import './Shop.css';
 import { Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faCartPlus, faStar } from "@fortawesome/free-solid-svg-icons";
 import { addToCart } from "../../../redux/actions/CartAction";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import SwiperCore, { Pagination, Navigation } from "swiper/core";
+import ProductModal from "../Home/ProductModal/ProductModal";
 SwiperCore.use([Pagination, Navigation]);
 
 const Shop = () => {
@@ -17,6 +19,15 @@ const Shop = () => {
     });
     const Dispatch = useDispatch();
 
+    const [productss, setProductss] = useState({});
+    const handleDetails = (id) => {
+        const productsDetails = products.find(x => x.key === id)
+        console.log(productsDetails);
+        setProductss(productsDetails)
+
+
+    }
+    console.log(productss);
     return (
         <div style={{}} className="">
             <h2>FEATURED PRODUCTS</h2>
@@ -31,11 +42,18 @@ const Shop = () => {
                 className="mySwiper row"
             >
                 {products.slice(1, 16).map((product) => (
-                    <SwiperSlide className=" col-md-4">
-                        <div>
-                            <Card style={{ width: "18rem" }}>
+                    <SwiperSlide className=" col-md-4 ">
+                        <div class="card">
+                            <Card style={{ width: "100%" }}>
                                 <Card.Img variant="top" src={product.img} />
+
                                 <Card.Body>
+                                    {/* <button class="button " data-bs-toggle="modal" data-bs-target="#exampleModalToggle" onClick={() => handleDetails(product.key)}>
+                                        <i className="btn btn-primary" style={{ fontSize: "24px" }}>Modal</i>
+                                    </button> */}
+                                    {/* <button class="button" onClick="deleteData()">
+                                        <span aria-hidden="true" style={{ fontSize: "40px" }}>&times;               </span>
+                                    </button>' */}
                                     <p>
                                         <small>{product.category}</small>
                                     </p>
@@ -47,18 +65,22 @@ const Shop = () => {
                                         {product.star}
                                         <FontAwesomeIcon icon={faStar} />
                                     </p>
-                                    <button
-                                        onClick={() => Dispatch(addToCart(product))}
-                                        className="btn btn-primary"
-                                    >
-                                        Add to cart
-                  </button>
+                                    <div>
+                                        <button
+                                            onClick={() => Dispatch(addToCart(product))}
+                                            className="btn btn-outline-dark"
+                                        ><FontAwesomeIcon icon={faCartPlus} />Add to cart </button>
+                                        <button className="btn btn-outline-dark ms-4" data-bs-toggle="modal" data-bs-target="#exampleModalToggle" onClick={() => handleDetails(product.key)}>View Details
+                                        </button>
+                                    </div>
                                 </Card.Body>
                             </Card>
                         </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <ProductModal handleDetails={handleDetails}
+                productinfo={productss}></ProductModal>
         </div>
     );
 };
