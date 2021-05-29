58703fef642eb346bc67df5dc92554dbdaa7bf7a
Tanvir Hossain Robin <tanvirhossain88221@gmail.com>
2021-05-29 15:41:22 +0600

    bug fix

--- src/components/Home/Shop/Shop.jsx
+++ src/components/Home/Shop/Shop.jsx
@@ -12,55 +12,55 @@ import SwiperCore, { Pagination, Navigation } from "swiper/core";
 SwiperCore.use([Pagination, Navigation]);
 
 const Shop = () => {
-  const products = useSelector((state) => {
-    return state.cart.fakeData;
-  });
-  const Dispatch = useDispatch();
+    const products = useSelector((state) => {
+        return state.cart.fakeData;
+    });
+    const Dispatch = useDispatch();
 
-  return (
-    <div style={{}} className="">
-        <h2>FEATURED PRODUCTS</h2>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        slidesPerGroup={3}
-        loop={true}
-        loopFillGroupWithBlank={true}
-        pagination={false}
-        navigation={true}
-        className="mySwiper row"
-      >
-        {products.slice(1, 16).map((product) => (
-          <SwiperSlide className=" col-md-4">
-            <div>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={product.img} />
-                <Card.Body>
-                  <p>
-                    <small>{product.category}</small>
-                  </p>
-                  <Card.Title>{product.name.slice(1, 40)}...</Card.Title>
-                  <h3>
-                    <b>Price: ${product.price}</b>
-                  </h3>
-                  <p>
-                    {product.star}
-                    <FontAwesomeIcon icon={faStar} />
-                  </p>
-                  <button
-                    onClick={() => Dispatch(addToCart(product))}
-                    className="btn btn-primary"
-                  >
-                    Add to cart
+    return (
+        <div style={{}} className="">
+            <h2>FEATURED PRODUCTS</h2>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                slidesPerGroup={3}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={false}
+                navigation={true}
+                className="mySwiper row"
+            >
+                {products.slice(1, 16).map((product) => (
+                    <SwiperSlide className=" col-md-4">
+                        <div>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={product.img} />
+                                <Card.Body>
+                                    <p>
+                                        <small>{product.category}</small>
+                                    </p>
+                                    <Card.Title>{product.name.slice(0, 40)}...</Card.Title>
+                                    <h3>
+                                        <b>Price: ${product.price}</b>
+                                    </h3>
+                                    <p>
+                                        {product.star}
+                                        <FontAwesomeIcon icon={faStar} />
+                                    </p>
+                                    <button
+                                        onClick={() => Dispatch(addToCart(product))}
+                                        className="btn btn-primary"
+                                    >
+                                        Add to cart
                   </button>
-                </Card.Body>
-              </Card>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
 };
 
 export default Shop;
